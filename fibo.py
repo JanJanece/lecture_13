0a09f16d76ee51df79d3fcafce7fec264d47c321
JanJanece <226390@vutbr.cz>
2021-05-04 17:23:28 +0200

    Je to hotovo

--- fibo.py
+++ fibo.py
@@ -1,7 +1,17 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    else:
+        return recursive_nth_fibo(n - 2) + recursive_nth_fibo(n - 1)
 
 def main():
-    pass
-
+    prvek = int(input("Zadejj prvek:"))
+    print(recursive_nth_fibo(prvek))
+    poslupnost = [recursive_nth_fibo(n) for n in range(prvek + 1)]
+    print(poslupnost)
 
 if __name__ == '__main__':
     main()
